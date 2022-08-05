ffea304674fb9ef13d67e8b4027bd6b77f711d7c
Muryllo <mumuryllo>
2022-08-05 18:28:40 -0300

    ggs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+//Comandos ng coloque primeiro no import
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
 })
-export class AppComponent {
-  title = 'meu-primeiro-projeto';
+
+export class AppComponent implements OnInit {
+  constructor(){ }
+  ngOnInit(): void {
+    setTimeout(()=>{
+      console.log(1);
+    },5000)
+  }
+
 }
